082a684457fcf06223985262b63d369355363e02
siddharth9890 <siddharth.a9890@gmail.com>
2022-04-05 18:49:49 +0530

    fixed minor bugs

--- index.ts
+++ index.ts
@@ -54,7 +54,19 @@ const corsOptions = {
   },
   options: ["POST", "GET"],
 };
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+app.use(function (req, res, next) {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://coding-machine.pages.dev/*"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST,DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(express.json({ limit: "5mb" }));
 app.use(coreRouter);
 
