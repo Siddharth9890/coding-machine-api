e3f180eae69841d7b1f6eb115792db53f9d071d4
siddharth9890 <siddharth.a9890@gmail.com>
2022-04-06 10:58:16 +0530

    fixed minor bugs

--- index.ts
+++ index.ts
@@ -27,7 +27,7 @@ const app = express();
 app.use(function (req, res, next) {
   res.header(
     "Access-Control-Allow-Origin",
-    "https://coding-machine.pages.dev/"
+    "https://coding-machine.pages.dev"
   );
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
   res.header(
