0dd8e99e59f977b1a83d94d1118e1b612cf5a72b
siddharth9890 <siddharth.a9890@gmail.com>
2022-04-06 07:33:53 +0530

    fixed minor bugs

--- index.ts
+++ index.ts
@@ -55,7 +55,15 @@ const corsOptions = {
   },
   methods: ["POST", "GET"],
 };
-app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST,DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.use(express.json({ limit: "5mb" }));
 app.use(coreRouter);
