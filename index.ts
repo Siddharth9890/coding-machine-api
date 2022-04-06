ee43cf7b126962e255e05c33c096774b19aac6be
siddharth9890 <siddharth.a9890@gmail.com>
2022-04-06 11:00:42 +0530

    fixed minor bugs

--- index.ts
+++ index.ts
@@ -24,39 +24,35 @@ const limiter = rateLimiter({
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-app.use(function (req, res, next) {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://coding-machine.pages.dev"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header(
+//     "Access-Control-Allow-Origin",
+//     "https://coding-machine.pages.dev"
+//   );
+//   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 
-// var whitelist = [
-//   "https://coding-machine.pages.dev/",
-//   "https://coding-machine.pages.dev/submit",
-//   "https://coding-machine.pages.dev/check-status",
-//   "https://coding-machine.pages.dev/result",
-// ];
-// var corsOptions = {
-//   origin: function (origin: any, callback: any) {
-//     if (whitelist.indexOf(origin) !== -1) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error("Not allowed by CORS"));
-//     }
-//   },
-//   methods: ["GET", "POST"],
-// };
+var whitelist = ["https://coding-machine.pages.dev"];
+var corsOptions = {
+  origin: function (origin: any, callback: any) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["GET", "POST"],
+  preflightContinue: true,
+};
 
 // Then pass them to cors:
 // app.options("*", cors({ preflightContinue: true }));
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 
 const DB = process.env.DATABASE!.replace(
   "<PASSWORD>",
